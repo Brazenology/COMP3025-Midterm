08e6fef6713591f165cd19eaaa080ab0a62b903a
student <student@students-mac.student.georgianc.on.ca>
2016-02-26 10:31:11 -0500

    begun swift programming

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -9,7 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //quantity labels
+    @IBOutlet weak var listItem1: UILabel!
+    @IBOutlet weak var listItem2: UILabel!
+    @IBOutlet weak var listItem3: UILabel!
+    @IBOutlet weak var listItem4: UILabel!
+    @IBOutlet weak var listItem5: UILabel!
+    
+    //variables
+    var listItemQuantity1 = 0
+    var listItemQuantity2 = 0
+    var listItemQuantity3 = 0
+    var listItemQuantity4 = 0
+    var listItemQuantity5 = 0
+    
+    //plus/minus buttons
+    
+    
+    @IBAction func minusItem1Click(sender: AnyObject) {
+        if (listItemQuantity1 > 0) {
+            listItemQuantity1--
+        }
+        listItem1.text = listItemQuantity1
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
